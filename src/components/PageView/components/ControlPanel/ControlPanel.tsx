63e82d6367c1ccff335dba0d3a81f50045850f63
Tuo Yuan <yuan@localhost.com>
2025-02-07 07:14:04 +0800

    feature/position: display current robot position and heading

--- src/components/PageView/components/ControlPanel/ControlPanel.tsx
+++ src/components/PageView/components/ControlPanel/ControlPanel.tsx
@@ -1,23 +1,31 @@
-import React from 'react'
+/** @format */
 
-const ControlPanel = ({position, moveRobot, getPosition}) => {
-  return (
-    <div>
-        <div className='control-robot'>
-            <button onClick={() => moveRobot("Move")}>Move</button>
-            <button onClick={() => moveRobot("Left")}>Left</button>
-            <button onClick={() => moveRobot("Right")}>Right</button>
-        </div>
-        <div className="report-position">
-            <button>Report</button>
-            <input 
-                style={{textAlign: "center"}}
-                placeholder="(X, Y, F)"
-                value={position} 
-                readOnly={true}></input>
-        </div>
-    </div>
-  )
+import React from "react";
+interface Props {
+  direction: "Up" | "Down" | "Left" | "Right";
+  position: number[];
+  moveRobot: (action: string) => void;
+  getPosition?: (e: Event) => void;
 }
+const ControlPanel: React.FC<Props> = ({ position, moveRobot, direction }) => {
+  return (
+    <div className="control-panel">
+      <div className="control-robot">
+        <button onClick={() => moveRobot("Move")}>Move</button>
+        <button onClick={() => moveRobot("Left")}>Left</button>
+        <button onClick={() => moveRobot("Right")}>Right</button>
+      </div>
+      <div className="report-position">
+        {/* <button onClick={getPosition}>Report</button> */}
+        <label>Position: </label>
+        <input
+          style={{ textAlign: "center" }}
+          placeholder="(X, Y, F)"
+          value={`${position},${direction}`}
+          readOnly={true}></input>
+      </div>
+    </div>
+  );
+};
 
-export default ControlPanel
+export default ControlPanel;
